1e3ed0ffd81eaa4bce700b0e006b39fe4d66ff0a
Tasminakhatri <fiwakhatri@gmail.com>
2025-09-18 17:42:43 +0545

    java-dataTypes

---
+++ DataTypes.java
@@ -0,0 +1,30 @@
+class DataTypes
+{
+    public static void main(String a[])
+    {
+        int num1 = 9;
+        byte by = 12;
+        short sh = 558;
+        long l = 5667;
+
+        float f = 6.5f;
+        double d = 5.8;
+
+        char c = '8';
+
+        boolean b = true;
+
+        // System.out.print("result");
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+    }
+
+}
+            
